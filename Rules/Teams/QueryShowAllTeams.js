6eede87c5d774ab46bae00c12002d6cc9358233a
Victor Hugo Cardoso <victor.cardoso@intelligenzait.com>
2024-12-18 23:45:58 +0000

    local removido - instrutor 1 na tela novamente

--- Rules/Teams/QueryShowAllTeams.js
+++ Rules/Teams/QueryShowAllTeams.js
@@ -3,5 +3,5 @@
  * @param {IClientAPI} clientAPI
  */
 export default function QueryShowAllTeams(clientAPI) {
-    return "?$expand=cust_ListaNav($expand=cust_AlunosNav),cust_Inst1Nav,cust_Inst2Nav&$filter=cust_LOCN_DESC ne null and externalName ne null and cust_LMS ne 'S' and cust_Status ne 'cancelada'&$orderby=cust_START_TME"
+    return "?$expand=cust_ListaNav($expand=cust_AlunosNav),cust_Inst1Nav,cust_Inst2Nav&$filter=externalName ne null and cust_LMS ne 'S' and cust_Status ne 'cancelada'&$orderby=cust_START_TME"
 }
