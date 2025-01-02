46bdd372dfa5547431a79f25da3beba28bc20a09
Caio Melo <caio.melo@lab2dev.com>
2025-01-02 15:42:11 +0000

    fix: adjust filter by team

--- Rules/Main/CalendarQuery.js
+++ Rules/Main/CalendarQuery.js
@@ -2,17 +2,39 @@
  * Describe this function...
  * @param {IClientAPI} clientAPI
  */
-export default function CalendarQuery(context) {
-    return defaultQuery(context)
-}
 
 function defaultQuery(context) {
     var cExpand = "&$expand=cust_Inst1Nav,cust_Inst2Nav"
     var dDate = new Date();
     var cDate = dDate.getFullYear().toString() + "-" + (dDate.getMonth() + 1).toString().padStart(2, "0") + "-" + dDate.getDate().toString().padStart(2, "0");
-    let cFilter = "$filter=cust_LMS ne 'S' and cust_Status ne 'cancelada' and externalName ne null and cust_START_TME ge " + cDate + "T00:00:00Z and cust_START_TME le " + cDate + "T23:59:59Z";
+    // let cFilter = "cust_LMS ne 'S' or cust_LMS eq null and cust_Status ne 'cancelada' or cust_Status eq null and externalName ne null and cust_START_TME ge " + cDate + "T00:00:00Z and cust_START_TME le " + cDate + "T23:59:59Z";
+    let dfilter = `$filter=(cust_START_TME ge 2025-01-02T00:00:00Z and cust_START_TME le 2025-01-02T23:59:59Z) and (cust_LMS ne 'S' or cust_LMS eq null) and (cust_Status ne 'cancelada' or cust_Status eq null) and (externalName ne null) and (cust_INST_ID1 eq 'SRMELLO')`
+    dfilter += cExpand
 
+    return dfilter;
+}
+
+export default async function CalendarQuery(context) {
+    var cExpand = "&$expand=cust_Inst1Nav,cust_Inst2Nav"
+    var dDate = new Date();
+    var cDate = dDate.getFullYear().toString() + "-" + (dDate.getMonth() + 1).toString().padStart(2, "0") + "-" + dDate.getDate().toString().padStart(2, "0");
+    // let cFilter = "cust_LMS ne 'S' or cust_LMS eq null and cust_Status ne 'cancelada' or cust_Status eq null and externalName ne null and cust_START_TME ge " + cDate + "T00:00:00Z and cust_START_TME le " + cDate + "T23:59:59Z";
+    
+    const IASUser = context.evaluateTargetPath("#Application/#AppData/UserId");
+    // const IASUser = 'SRMELLO'
+
+    const query = `$filter=externalCode eq '${IASUser}' or cust_RELATED_USER eq '${IASUser}'`;
+
+    const response = await context.read(
+        "/Attendance_List/Services/CAP_SERVICE_SF_LMS.service",
+        "cust_Instrutores",
+        ["externalCode", "cust_RELATED_USER"],
+        query
+    );
+    const ExtCode = response.find(i => i.externalCode)?.externalCode || '';
+    const SFUser = response.find(i => i.cust_RELATED_USER)?.cust_RELATED_USER || '';
+
+    let cFilter = `$filter=(cust_START_TME ge ${cDate}T00:00:00Z and cust_START_TME le ${cDate}T23:59:59Z) and (cust_LMS ne 'S' or cust_LMS eq null) and (cust_Status ne 'cancelada' or cust_Status eq null) and (externalName ne null) and (cust_INST_ID1 eq '${ExtCode}' or cust_INST_ID1 eq '${SFUser}')`
     cFilter += cExpand
-
-    return cFilter;
+    return cFilter
 }
--- Rules/Main/UpdateQueryWithSelectedDate.js
+++ Rules/Main/UpdateQueryWithSelectedDate.js
@@ -1,13 +1,31 @@
-export default function UpdateQueryWithSelectedDate(clientAPI) {
+
+export default async function UpdateQueryWithSelectedDate(clientAPI) {
     try {
+        const IASUser = clientAPI.evaluateTargetPath("#Application/#AppData/UserId");
+        // const IASUser = 'SRMELLO'
+
+        const query = `$filter=externalCode eq '${IASUser}' or cust_RELATED_USER eq '${IASUser}'`;
+
+        const response = await clientAPI.read(
+            "/Attendance_List/Services/CAP_SERVICE_SF_LMS.service",
+            "cust_Instrutores",
+            ["externalCode", "cust_RELATED_USER"],
+            query
+        );
+        const ExtCode = response.find(i => i.externalCode)?.externalCode || '';
+        const SFUser = response.find(i => i.cust_RELATED_USER)?.cust_RELATED_USER || '';
+
         let selectedDate = clientAPI.getPageProxy().getControl("SectionedTable0").getSection("SectionCalendar1").getSelectedDate();
         let year = selectedDate.getFullYear();
         let month = ("0" + (selectedDate.getMonth() + 1)).slice(-2);
         let day = ("0" + selectedDate.getDate()).slice(-2);
         let formattedDate = year + '-' + month + '-' + day;
         var cExpand = "&$expand=cust_Inst1Nav,cust_Inst2Nav"
-        let filterQuery = "$filter=cust_LMS ne 'S' and cust_Status ne 'cancelada' and externalName ne null and cust_START_TME ge " + formattedDate + "T00:00:00Z and cust_START_TME le " + formattedDate + "T23:59:59Z";
+        let filterQuery = `$filter=(cust_START_TME ge ${formattedDate}T00:00:00Z and cust_START_TME le ${formattedDate}T23:59:59Z) and (cust_LMS ne 'S' or cust_LMS eq null) and (cust_Status ne 'cancelada' or cust_Status eq null) and (externalName ne null) and (cust_INST_ID1 eq '${ExtCode}' or cust_INST_ID1 eq '${SFUser}')`;
+
         filterQuery += cExpand
+
+        alert(JSON.stringify(filterQuery, null, 2))
         let oCardObj = clientAPI.getPageProxy().getControl("SectionedTable0").getSection("SectionObjectCardCollection1");
         let oTarget = oCardObj.getTargetSpecifier();
         oTarget.setQueryOptions(filterQuery);
@@ -16,3 +34,22 @@ export default function UpdateQueryWithSelectedDate(clientAPI) {
         alert("An error occurred: " + error.message);
     }
 }
+
+// export default function UpdateQueryWithSelectedDate(clientAPI) {
+//     try {
+//         let selectedDate = clientAPI.getPageProxy().getControl("SectionedTable0").getSection("SectionCalendar1").getSelectedDate();
+//         let year = selectedDate.getFullYear();
+//         let month = ("0" + (selectedDate.getMonth() + 1)).slice(-2);
+//         let day = ("0" + selectedDate.getDate()).slice(-2);
+//         let formattedDate = year + '-' + month + '-' + day;
+//         var cExpand = "&$expand=cust_Inst1Nav,cust_Inst2Nav"
+//         let filterQuery = "$filter=cust_LMS ne 'S' and cust_Status ne 'cancelada' and externalName ne null and cust_START_TME ge " + formattedDate + "T00:00:00Z and cust_START_TME le " + formattedDate + "T23:59:59Z";
+//         filterQuery += cExpand
+//         let oCardObj = clientAPI.getPageProxy().getControl("SectionedTable0").getSection("SectionObjectCardCollection1");
+//         let oTarget = oCardObj.getTargetSpecifier();
+//         oTarget.setQueryOptions(filterQuery);
+//         oCardObj.setTargetSpecifier(oTarget);
+//     } catch (error) {
+//         alert("An error occurred: " + error.message);
+//     }
+// }
